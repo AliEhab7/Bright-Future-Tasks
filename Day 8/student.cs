c5d5262f6e841177812253b28cbfa8c1d4b30780
Ali Ehab <bettercalllevmz@gmail.com>
2026-05-03 19:58:55 +0300

    Create student.cs

---
+++ Day 8/student.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet_task1
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public DateTime DateOfBirth { get;set; }
+        public Student() { }
+
+    }
+}
